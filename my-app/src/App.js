7d312e076baad3bbbe012ce7912f9bab82f6c7df
samuel davies <sjdavies24@hotmail.com>
2022-08-21 15:10:32 +1000

    master branch - video to gif example

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,14 +4,11 @@ import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg';
 
 const ffmpeg = createFFmpeg({ log: true });
 
-const setMode = 2;
-
 function App() {
 
   const [ ready, setReady ] = useState(false);
   const [ video, setVideo ] = useState();
   const [ gif, setGif ] = useState();
-  const [ images, setImages ] = useState();
 
   const load = async () => {
     await ffmpeg.load();
@@ -32,60 +29,19 @@ function App() {
     const url = URL.createObjectURL(new Blob([data.buffer], { type: 'image/gif' }));
     setGif(url);
   }
-
-  const convertToImages = async () => {
-    ffmpeg.FS('writeFile', `image.gif`, await fetchFile(gif));
-
-    // console.defaultLog = console.log.bind(console);
-    // console.logs = [];
-    // console.log = function () {
-    //   console.defaultLog.apply(console, arguments);
-    //   console.logs.push(Array.from(arguments));
-    // }
-
-    await ffmpeg.run('-i', 'image.gif','-vsync', '0', '-progress', '-', 'frame%d.png');
-
-    const maxFrames = console.logs.filter(e => e.includes('frame='));
-    console.defaultLog(`mf`, maxFrames);
-
-    const data = ffmpeg.FS('readFile', 'frame1.png');
-
-    const url = URL.createObjectURL(new Blob([data.buffer], { type: 'image/gif' }));
-    setImages(url);
-  }
-
-  const onChangeHandler = {
-    gifLoad: (e) => setGif(URL.createObjectURL(e.target.files?.item(0))),
-    videoLoad: (e) => setVideo(URL.createObjectURL(e.target.files?.item(0))),
-  }
-
-  const mode = {
-    1: {
-      load: [video, 'videoLoad'],
-      set: [gif, setGif],
-      convert: convertToGif
-    },
-    2: {
-      load: [gif, 'gifLoad'],
-      set: [images, setImages],
-      convert: convertToImages
-    }
-  }
-
-  const loadElement = setMode === 1 ? <video controls width="250" src={video}/> : <img src={gif} width="250" alt="gif"/>;
   
   return ready ? (
     <div className="App">
 
-      { mode[setMode]['load'][0] && loadElement  }
+      { video && <video controls width="250" src={video}/>  }
       
-      <input type="file" onChange={onChangeHandler[mode[setMode]['load'][1]]}/>
+      <input type="file" onChange={e => setVideo(URL.createObjectURL(e.target.files?.item(0)))}/>
 
       <h3>Result</h3>
       
-      <button onClick={mode[setMode]['convert']}>Convert</button>
+      <button onClick={convertToGif}>Convert</button>
       
-      { mode[setMode]['set'][0] && <img src={mode[setMode]['set'][0]} width="250" alt={setMode === 1 ? "gif" : "pngs"}/> }
+      { gif && <img src={gif} width="250" alt="gif"/> }
     </div> 
   ) : <p>Loading...</p>;
 }
